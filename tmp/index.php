f2839af5a6a726ef0b859a935623371d1e556a57
ovivic <ovidiuz81@yahoo.com>
2021-11-10 15:18:46 +0000

    finished rough design for the homepage

--- tmp/index.php
+++ tmp/index.php
@@ -7,6 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
 
     <link rel="stylesheet" href="../assets/styles/style.css">
 
@@ -14,7 +15,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light fine-dining-navbar">
+    <nav class="navbar navbar-expand-lg navbar-light fd-navbar">
         <div class="container">
             <a class="navbar-brand" href="#">FINE DINING</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,16 +38,215 @@
         </div>
     </nav>
 
-    <div class="jumbotron jumbotron-fluid fine-dining-jumbotron">
-        <div class="fine-dining-homepage-form-container">
-
+    <div class="jumbotron jumbotron-fluid d-none d-md-block fd-jumbotron d-md-none">
+        <div class="fd-homepage-form text-center">
+            <h1>Eat in or takeaway?</h1>
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-4">
+                    <a class="btn btn-primary fd-homepage-form-button" href="#">BOOK TABLE</a>
+                </div>
+                <div class="col-md-4">
+                    <a class="btn btn-primary fd-homepage-form-button" href="#">FIND DELIVERY</a>
+                </div>
+                <div class="col-md-2"></div>
+            </div>
+<!--            <div class="row">-->
+<!--                <div class="col-xs-12">-->
+<!--                    <form>-->
+<!--                        <div class="form-row align-items-center">-->
+<!--                            <div class="col-auto">-->
+<!--                                <label class="sr-only" for="inlineFormInputGroup">Username</label>-->
+<!--                                <div class="input-group mb-2">-->
+<!--                                    <div class="input-group-prepend">-->
+<!--                                        <div class="input-group-text">@</div>-->
+<!--                                    </div>-->
+<!--                                    <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Username">-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                            <div class="col-auto">-->
+<!--                                <button type="submit" class="btn btn-primary mb-2">Submit</button>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </form>-->
+<!--                </div>-->
+<!--            </div>-->
         </div>
     </div>
 
+<!--  UPDATE THE FORM HERE  -->
+    <div class="d-md-none fd-sm-homepage-form">
+        <h1>Eat in or takeaway?</h1>
+    </div>
 
+    <div class="container fd-container">
+        <h1 class="text-center">Choose a dish</h1>
+        <div class="row fd-restaurant-card-container">
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h1 class="text-center">Your favourites</h1>
+        <div class="row fd-restaurant-card-container">
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <img class="card-img-top" src="https://dummyimage.com/600x200/000/fff.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <img class="card-img-top" src="https://dummyimage.com/600x200/000/fff.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <img class="card-img-top" src="https://dummyimage.com/600x200/000/fff.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h1 class="text-center">Our featured restaurants</h1>
+        <div class="row fd-restaurant-card-container">
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <img class="card-img-top" src="https://dummyimage.com/600x200/000/fff.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <img class="card-img-top" src="https://dummyimage.com/600x200/000/fff.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <img class="card-img-top" src="https://dummyimage.com/600x200/000/fff.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h1 class="text-center">Top Rated</h1>
+        <div class="row fd-restaurant-card-container">
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <img class="card-img-top" src="https://dummyimage.com/600x200/000/fff.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <img class="card-img-top" src="https://dummyimage.com/600x200/000/fff.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card fd-restaurant-card">
+                    <img class="card-img-top" src="https://dummyimage.com/600x200/000/fff.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <div class="container">
-
+    <div class="fd-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h2 class="fd-footer-heading">Customer Support</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#"><i class="bi-alarm"></i>Contact us</a></li>
+                        <li><a href="#">Log in</a></li>
+                        <li><a href="#">Sign up</a></li>
+                        <li><a href="#">My account</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h2 class="fd-footer-heading">Top Cuisines</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Thai</a></li>
+                        <li><a href="#">Chinese</a></li>
+                        <li><a href="#">Indian</a></li>
+                        <li><a href="#">American</a></li>
+                        <li><a href="#">Pizza</a></li>
+                        <li><a href="#">Sushi</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h2 class="fd-footer-heading">Social Media</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#"><i class="bi bi-facebook"></i> Facebook</a></li>
+                        <li><a href="#"><i class="bi bi-instagram"></i> Instagram</a></li>
+                        <li><a href="#"><i class="bi bi-twitter"></i> Twitter</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center fd-footer-copyright">
+                    <p>
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</a>
+                    </p>
+                </div>
+            </div>
     </div>
 
     <!-- Optional JavaScript -->
